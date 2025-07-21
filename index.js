ca42ee1077f5ab0b6aa022fc0115e304aae50501
Shrouk Menesy <menesyshrouk@gmai.com>
2025-07-21 18:24:50 +0300

    .

---
+++ index.js
@@ -0,0 +1,9 @@
+// كود JavaScript
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("active");
+}
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  sidebar.classList.toggle("active");
+}
