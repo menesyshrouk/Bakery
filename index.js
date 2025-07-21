5b7ed4666c4058b3d8959e4175fa6086ceea3284
Shrouk Menesy <menesyshrouk@gmai.com>
2025-07-21 18:57:29 +0300

    edit

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// كود JavaScript
-function toggleSidebar() {
-  const sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("active");
-}
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("active");
